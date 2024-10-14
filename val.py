5e7702d5d1b405b1968bc25316b58c883308c377
LazyShark2001 <141640045+LazyShark2001@users.noreply.github.com>
2024-10-14 17:29:03 +0800

    Update val.py

--- val.py
+++ val.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
 
-    model = YOLO('weight/FDADNet.pt')
+    model = YOLO('FDADNet.pt')
 
     metrics = model.val(split='test', iou=0.5, batch=1, data='WBP-DET/data.yaml')
